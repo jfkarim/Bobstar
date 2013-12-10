9f85fb17c95f5a55a820fb82d18755974bbbdef0
Jayan Fazal Karim <jfkarim@Jayans-MacBook-Pro.local>
2013-12-10 09:04:17 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bobstar::Application.config.secret_token = '8d138ee1f9e20300396fea1ecfcf9c0f36a413b27ff8dea47d3c6815a19f2651095217b2b6a1de5a4f0b0198be2e7a8562a68dec15ce1c3dd0fc943c4e49f973'
